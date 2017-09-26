4ce4d0f32a00639837533075c36ce374c995aba1
b3ndill <b3ndill@gmail.com>
2017-09-26 23:22:56 +0000

    Added Deck class with randomization

--- blackjack.py
+++ blackjack.py
@@ -1,20 +1,25 @@
-import random
+from random import shuffle
 
 class Card(object):
     """docstring for Card."""
-    suits=("Hearts", "Spades", "Diamonds", "Clubs")
-    def __init__(self, number, suit):
-        self.number = number
+    def __init__(self, suit, rank):
         self.suit = suit
-
+        self.rank = rank
     def __str__(self):
-        labels = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
-        return("{0} of {1}".format(str(labels[self.number]), self.suit))
+        suits=("","Hearts", "Spades", "Diamonds", "Clubs")
+        ranks = ("","Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+        return("{0} of {1}".format(str(ranks[self.rank]), str(suits[self.suit])))
 
 class Deck(object):
     """docstring for Deck."""
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self):
+        self.deck = [(x, y) for x in range(1,5) for y in range(1,14)]
+        for i in range(0, 5):
+            shuffle(self.deck)
+    def __iter__(self):
+        return iter(self.deck)
+    def draw(self):
+         return self.deck.pop()
 
 class Player(object):
     """docstring for Player."""
@@ -28,6 +33,3 @@ class Hand(object):
     """docstring for Hand."""
     def __init__(self, arg):
         self.arg = arg
-
-x = Card(11, "Hearts")
-print(x)
